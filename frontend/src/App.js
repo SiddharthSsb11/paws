5746d103372323ca4bd43651451348e252307c85
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-03-18 03:13:29 +0530

    favicon changed and react files added

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2 style={{textAlign: 'center', color: 'purple'}}>PAWS</h2>
+    </div>
+  );
+}
+
+export default App;
