0d35c92aeecf66354ce5ad5341acc43cdd4e9364
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-03-18 20:37:52 +0530

    styling edited

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import "./App.css";
+import React, { Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import DashboardPage from "./Pages/DashboardPage";
+import OnBoardingPage from "./Pages/OnBoardingPage";
 
 function App() {
   return (
     <div className="App">
-      <h2 style={{textAlign: 'center', color: 'purple'}}>PAWS</h2>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/onboarding" element={<OnBoardingPage />} />
+      </Routes>
     </div>
   );
 }
