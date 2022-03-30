21cc4f7ba0d77434b4412479d57b5e41a6877b19
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-03-30 23:58:07 +0530

    edited dashboard

--- frontend/src/Pages/DashboardPage.js
+++ frontend/src/Pages/DashboardPage.js
@@ -1,78 +1,20 @@
 import { Box, IconButton } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { CloseIcon } from "@chakra-ui/icons";
 
 import "./DashboardPage.css";
-import TinderCard from "react-tinder-card";
 import MatchContainer from "../components/MatchContainer";
+import SwipeContainer from "../components/SwipeContainer";
+import ChatContainer from "../components/ChatContainer";
 
-const characters = [
-  {
-    name: "Richard Hendricks",
-    url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/medium-sized-dogs-1613083812.jpg?crop=0.668xw:1.00xh;0.0369xw,0&resize=640:*",
-  },
-  {
-    name: "Erlich Bachman",
-    url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/medium-sized-dogs-1613083812.jpg?crop=0.668xw:1.00xh;0.0369xw,0&resize=640:*",
-  },
-  {
-    name: "Monica Hall",
-    url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/medium-sized-dogs-1613083812.jpg?crop=0.668xw:1.00xh;0.0369xw,0&resize=640:*",
-  },
-  {
-    name: "Jared Dunn",
-    url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/medium-sized-dogs-1613083812.jpg?crop=0.668xw:1.00xh;0.0369xw,0&resize=640:*",
-  },
-  {
-    name: "Dinesh Chugtai",
-    url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/medium-sized-dogs-1613083812.jpg?crop=0.668xw:1.00xh;0.0369xw,0&resize=640:*",
-  },
-];
 
 const DashboardPage = () => {
-  const [lastDirection, setLastDirection] = useState();
 
-  const swiped = (direction, nameToDelete) => {
-    console.log("removing: " + nameToDelete);
-    setLastDirection(direction);
-  };
-  console.log(lastDirection);
-  const outOfFrame = (name) => {
-    console.log(name + " left the screen!");
-  };
   return (
     <div className="back">
-      <Box margin="auto">
+      <Box margin="auto" d="flex" >
         <MatchContainer />
-        <div className="swipeContainer">
-          <div className="cardContainer">
-            {characters.map((character) => (
-              <Box d="flex">
-                <TinderCard
-                  className="swipe"
-                  key={character.name}
-                  onSwipe={(dir) => swiped(dir, character.name)}
-                  onCardLeftScreen={() => outOfFrame(character.name)}
-                >
-                  <div
-                    style={{ backgroundImage: "url(" + character.url + ")" }}
-                    className="card"
-                  >
-                    <h3>{character.name}</h3>
-                  </div>
-                </TinderCard>
-              </Box>
-            ))}
-          </div>
-          <div className="swipeInfo">
-          {lastDirection ? (
-            <h2 className="infoText">You swiped {lastDirection}</h2>
-          ) : (
-            <h2>Swipe/Drag left or right</h2>
-          )}
-          </div>
-          
-        </div>
+        <SwipeContainer/>
+        <ChatContainer/>
       </Box>
     </div>
   );
--- frontend/src/components/SwipeContainer.js
+++ frontend/src/components/SwipeContainer.js
@@ -1,8 +1,73 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Box, IconButton } from "@chakra-ui/react";
+import TinderCard from "react-tinder-card";
+import './SwipeContainer.css'
+
+const characters = [
+    {
+      name: "Richard Hendricks",
+      url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/medium-sized-dogs-1613083812.jpg?crop=0.668xw:1.00xh;0.0369xw,0&resize=640:*",
+    },
+    {
+      name: "Erlich Bachman",
+      url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/medium-sized-dogs-1613083812.jpg?crop=0.668xw:1.00xh;0.0369xw,0&resize=640:*",
+    },
+    {
+      name: "Monica Hall",
+      url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/medium-sized-dogs-1613083812.jpg?crop=0.668xw:1.00xh;0.0369xw,0&resize=640:*",
+    },
+    {
+      name: "Jared Dunn",
+      url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/medium-sized-dogs-1613083812.jpg?crop=0.668xw:1.00xh;0.0369xw,0&resize=640:*",
+    },
+    {
+      name: "Dinesh Chugtai",
+      url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/medium-sized-dogs-1613083812.jpg?crop=0.668xw:1.00xh;0.0369xw,0&resize=640:*",
+    },
+  ];
 
 const SwipeContainer = () => {
+
+    const [lastDirection, setLastDirection] = useState();
+
+    const swiped = (direction, nameToDelete) => {
+      console.log("removing: " + nameToDelete);
+      setLastDirection(direction);
+    };
+    console.log(lastDirection);
+    const outOfFrame = (name) => {
+      console.log(name + " left the screen!");
+    };
   return (
-    <div>SwipeContainer</div>
+    <div className="swipeContainer">
+          <div className="cardContainer">
+            {characters.map((character) => (
+              <Box d="flex">
+                <TinderCard
+                  className="swipe"
+                  key={character.name}
+                  onSwipe={(dir) => swiped(dir, character.name)}
+                  onCardLeftScreen={() => outOfFrame(character.name)}
+                >
+                  <div
+                    style={{ backgroundImage: "url(" + character.url + ")" }}
+                    className="card"
+                  >
+                    <h3>{character.name}</h3>
+                  </div>
+                </TinderCard>
+              </Box>
+            ))}
+          </div>
+          <div className="swipeInfo">
+          {lastDirection ? (
+            <h2 className="infoText">You swiped {lastDirection}</h2>
+          ) : (
+            <h2>Swipe/Drag left or right</h2>
+          )}
+          </div>
+          
+        </div>
   )
 }
 
