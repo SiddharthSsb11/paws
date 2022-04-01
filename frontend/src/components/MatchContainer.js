774ef966681514a779eff32c9ac76ff6ec7a48e4
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-04-01 23:57:12 +0530

    code refactored

--- frontend/src/components/MatchContainer.js
+++ frontend/src/components/MatchContainer.js
@@ -1,12 +1,27 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const MatchContainer = () => {
   return (
-    <Box d="flex" flexDir="column" gap="0.3em" bg="purple.900" maxH="40rem"
+    <Box d="flex" flexDir="column" gap="0.3em" bg="purple.900" height="41rem" width="22. 5%"
+      color="white" 
+      borderRadius="7px" border="1.5px solid black" 
 
     >
-    matchCOntainer
+      <Box d="flex" justifyContent="space-between" borderBottom ="4px solid black" alignItems="center" width="100%">
+
+
+      </Box>
+
+      <Box d="flex" alignItems="center" gap="0.3em">
+        <Text>
+
+
+        </Text>
+
+      </Box>
+
+
     </Box>
   )
 }
