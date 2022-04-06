ae683e28e25e12dbdaa40239384590d1a55f3284
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-04-06 23:50:52 +0530

    chat container ui code done

--- frontend/src/components/ChatContainer.js
+++ frontend/src/components/ChatContainer.js
@@ -13,106 +13,193 @@ import {
   useDisclosure,
   ModalCloseButton,
   Button,
+  Text,
 } from "@chakra-ui/react";
+import { BsChatRightDotsFill, BsChatLeftDotsFill } from "react-icons/bs";
 import React, { useState } from "react";
 import { IoArrowBackCircleSharp } from "react-icons/io5";
+import { GiBalloonDog, GiDogHouse, GiJumpingDog } from "react-icons/gi";
 
 const ChatContainer = () => {
   const [selectedMatch, setSelectedMatch] = useState(true);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   return (
-    <Box
-      d="flex"
-      flexDir="column"
-      gap="1em"
-      bg="purple.900"
-      height="42rem"
-      width="25%"
-      //color="white"
-      borderRadius="7px"
-      border="1.5px solid black"
-      boxShadow="5px 5px 5px black"
-    >
-      <Box
-        d="flex"
-        justifyContent="space-around"
-        borderBottom="4px solid black" /* #9B2C2C */
-        alignItems="center"
-        width="100%"
-        height="16%"
-        bg="yellow.400"
-        borderRadius="5px"
-      >
-        <IconButton
-          variant="ghost"
-          //bg="gray.700"
-          color="gray.800"
-          size="md"
-          textAlign="center"
-          fontSize="4xl"
-          fontWeight="bold"
-          _hover={{ backgroundColor: "black", color: "red.600" }}
-          aria-label="Go Back to Swiping"
-          icon={<IoArrowBackCircleSharp />}
-          onClick={() => setSelectedMatch(false)}
-        />
-
-        <Badge
-          fontFamily="suez one"
-          variant="solid"
-          bg="black"
-          color="red.600"
-          fontSize="3xl"
-          px={3}
-          py={1}
+    <React.Fragment>
+      {selectedMatch ? (
+        <Box
+          d="flex"
+          flexDir="column"
+          gap="1em"
+          bg="purple.900"
+          height="42rem"
+          width="25%"
+          //color="white"
           borderRadius="7px"
-          letterSpacing="1.75px"
-          _hover={{ background: "red.600", color: "white" }}
-          //_hover={{ background: "black", color:"red.600"  }}
+          border="1.5px solid black"
+          boxShadow="5px 5px 5px black"
         >
-          Kayla
-        </Badge>
-        <IconButton
-          variant="ghost"
-          //bg="gray.700"
-          color="gray.800"
-          size="md"
-          textAlign="center"
-          fontSize="4xl"
-          fontWeight="bold"
-          _hover={{ backgroundColor: "black", color: "red.600" }}
-          aria-label="View Details"
-          //d={{ base: "flex" }}
-          icon={<ViewIcon />}
-          onClick={onOpen}
-        />
+          <Box
+            d="flex"
+            justifyContent="space-around"
+            borderBottom="4px solid black" /* #9B2C2C */
+            alignItems="center"
+            width="100%"
+            height="16%"
+            bg="yellow.400"
+            borderRadius="5px"
+          >
+            <IconButton
+              variant="ghost"
+              //bg="gray.700"
+              color="gray.800"
+              size="md"
+              textAlign="center"
+              fontSize="4xl"
+              fontWeight="bold"
+              _hover={{ backgroundColor: "black", color: "red.600" }}
+              aria-label="Go Back to Swiping"
+              icon={<IoArrowBackCircleSharp />}
+              onClick={() => setSelectedMatch(false)}
+            />
 
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Modal Title</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>hello profimle modal</ModalBody>
+            <Badge
+              fontFamily="suez one"
+              variant="solid"
+              bg="black"
+              color="red.600"
+              fontSize="3xl"
+              px={3}
+              py={1}
+              borderRadius="7px"
+              letterSpacing="1.75px"
+              _hover={{ background: "red.600", color: "white" }}
+              //_hover={{ background: "black", color:"red.600"  }}
+            >
+              Kayla
+            </Badge>
+            <IconButton
+              variant="ghost"
+              //bg="gray.700"
+              color="gray.800"
+              size="md"
+              textAlign="center"
+              fontSize="4xl"
+              fontWeight="bold"
+              _hover={{ backgroundColor: "black", color: "red.600" }}
+              aria-label="View Details"
+              //d={{ base: "flex" }}
+              icon={<ViewIcon />}
+              onClick={onOpen}
+            />
 
-            <ModalFooter>
-              <Button colorScheme="blue" mr={3} onClick={onClose}>
-                Close
-              </Button>
-              <Button variant="ghost">Secondary Action</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </Box>
+            <Modal isOpen={isOpen} onClose={onClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>hello profimle modal</ModalBody>
 
-      <Box d="flex" flexDir="column" alignItems="center" gap="1.2rem">
-      <Badge variant="outline" colorScheme="yellow" fontSize="xl" fontFamily="bungee" px={1.5}
-      letterSpacing="1.25px"
-    >
-    Chats
-    </Badge>
-      </Box>
-    </Box>
+                <ModalFooter>
+                  <Button colorScheme="blue" mr={3} onClick={onClose}>
+                    Close
+                  </Button>
+                  <Button variant="ghost">Secondary Action</Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
+          </Box>
+
+          <Box d="flex" flexDir="column" alignItems="center" gap="1.2rem">
+            <Badge
+              variant="outline"
+              colorScheme="yellow"
+              fontSize="xl"
+              fontFamily="bungee"
+              px={1.5}
+              letterSpacing="1.25px"
+            >
+              Chats
+            </Badge>
+          </Box>
+        </Box>
+      ) : (
+        <Box
+          flexDir="column"
+          d="flex"
+          alignItems="center"
+          //justifyContent="space-around"
+          bg="purple.900"
+          height="42rem"
+          width="25%"
+          gap="3em"
+          //color="white"
+          borderRadius="7px"
+          border="1.5px solid black"
+          boxShadow="5px 5px 5px black"
+        >
+          <Box
+            d="flex"
+            justifyContent="center"
+            borderBottom="4px solid black" /* #9B2C2C */
+            alignItems="center"
+            width="100%"
+            height="16%"
+            bg="yellow.400"
+            borderRadius="5px"
+          >
+            <Badge
+              fontFamily="suez one"
+              variant="solid"
+              bg="black"
+              color="red.600"
+              fontSize="3xl"
+              px={3}
+              py={1}
+              borderRadius="7px"
+              letterSpacing="1.75px"
+              _hover={{ background: "red.600", color: "white" }}
+              //_hover={{ background: "black", color:"red.600"  }}
+            >
+              
+              &nbsp;  Inbox &nbsp;
+             
+            </Badge>
+          </Box>
+
+          <Box
+            bg="yellow.300"
+            height="33%"
+            width="90%"
+            borderRadius="7px"
+            border="1.5px solid black"
+            d="flex"
+            justifyContent="center"
+            alignItems="center"
+            
+          >
+            <Badge
+              fontFamily="suez one"
+              variant="solid"
+              bg="red.600"
+              color="white"
+              fontSize="2xl"
+              px={3}
+              py={1}
+              borderRadius="7px"
+              letterSpacing="1.75px"
+              _hover={{ background: "red.600", color: "white" }}
+              textAlign="center"
+            >
+              Click on a Match
+              <br /> to start chatting
+            </Badge>
+          </Box>
+
+          <Icon color="yellow.500" h={48} w={48} as ={GiBalloonDog}></Icon>
+        </Box>
+      )}
+    </React.Fragment>
   );
 };
 
