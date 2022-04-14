00d8a7b8601978be33801e91ba69d4623de2f973
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-04-14 17:43:49 +0530

    alert comp added as a fallback

--- frontend/src/Pages/DashboardPage.js
+++ frontend/src/Pages/DashboardPage.js
@@ -1,25 +1,66 @@
 import { Box, IconButton } from "@chakra-ui/react";
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 
 import "./DashboardPage.css";
 import MatchContainer from "../components/MatchContainer";
 import SwipeContainer from "../components/SwipeContainer";
 import ChatContainer from "../components/ChatContainer";
-
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import PawsContext from "../Context/paws-context";
+import Alerts from "../components/Alerts";
 
 const DashboardPage = () => {
+  //const [user, setUser] = useState(null);
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+  const userId = cookies.UserId;
+
+  const { user } = useContext(PawsContext);
+
+  const getUser = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/user", {
+        params: { userId },
+      });
+      console.log("user loaded from server on dashboard",response.data)
+      //setUser(response.data);
+      //window.location.reload
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  //console.log("user", user);
 
   return (
-    <div className="back">
-      <Box  d="flex" alignItems="center" width="100%"  margin="1rem" justifyContent="space-around"
-        px={1} py={1}
-      >
-        <MatchContainer />
-        <SwipeContainer/>
-        <ChatContainer/>
-      </Box>
+    <React.Fragment>
+    {!user ? (
+      <Box className="home"><Alerts/></Box>
+      
+    ):(
+      <div className="back">
+        <Box
+          d="flex"
+          alignItems="center"
+          width="100%"
+          margin="1rem"
+          justifyContent="space-around"
+          px={1}
+          py={1}
+        >
+          <MatchContainer /* user = {user} *//>
+          <SwipeContainer />
+          <ChatContainer />
+        </Box>
     </div>
-  );
+
+    )}
+    </React.Fragment>
+      );
 };
 
 export default DashboardPage;
